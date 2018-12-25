9c1d1f6c71dd5c957acce103cebecdec7dddf58e
Ireneusz Larysz <ireneuszlarysz@gmail.com>
2018-12-25 18:52:54 +0100

    Added indicators

--- app.R
+++ app.R
@@ -21,7 +21,7 @@ ui <- fluidPage(
                      start = "2010-08-01", 
                      end = as.character(Sys.Date())),
       
-      br(),
+      tags$h5(strong("Additional plot features")),
       
       checkboxInput("log", "Plot y axis on log scale", 
                     value = FALSE),
@@ -29,8 +29,9 @@ ui <- fluidPage(
       checkboxInput("adjust", 
                     "Adjust prices for inflation", value = FALSE),
       
-      checkboxInput("MACD",
-                    "Show MACD indicator", value = FALSE),
+      radioButtons("price_indicator", label = ("Add indicator to price chart"),
+                   choices = list("MACD" = "MACD", "RSI" = "RSI", "TDI" = "TDI", "None" = "None"), 
+                   selected = "None"),
       
       selectInput("price_chart_type", "Choose chart type:",
                   choices = list("Candles" = "candlesticks",
@@ -83,9 +84,16 @@ server <- function(input, output) {
     
     chartSeries(data, theme = chartTheme(theme = "white", bg.col="white", major.tick="black", minor.tick="white"),
                 type = input$price_chart_type, log.scale = input$log, TA = NULL, name = input$symb)
-    if(input$MACD) addMACD()
+    
+    if(input$price_indicator == "MACD"){
+      addMACD()      
+    }else if(input$price_indicator == "RSI"){
+      addRSI()
+    }else if(input$price_indicator == "TDI"){
+      addTDI()
+    }
   })
-  
+    
   output$macro_chart <- renderPlot({
     macro_chart(indicator = input$indicator, 
       min_date = input$dates[1], max_date = input$dates[2], transformation = input$chart_transformation)
